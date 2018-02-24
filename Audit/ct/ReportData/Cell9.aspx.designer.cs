553a9bc2823f60c838dcee8c43c6b138ebfdc0a0
wy123456 <1575785605@qq.com>
2018-02-24 11:40:14 +0800

    修改

--- Audit/ct/ReportData/Cell9.aspx.designer.cs
+++ Audit/ct/ReportData/Cell9.aspx.designer.cs
@@ -19,6 +19,6 @@ namespace Audit.ct.ReportData {
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
+        protected global::System.Web.UI.HtmlControls.HtmlHead Head9;
     }
 }
