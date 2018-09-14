f447ece6535a98f021d06eea52e3db374932e9c8
luchg321 <36101261+luchg321@users.noreply.github.com>
2018-09-14 20:01:05 +0800

    11111111111111

---
+++ Audit/JumpDrpLogin.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="JumpDrpLogin.aspx.cs" Inherits="Audit.JumpDrpLogin" %>
+
+<!DOCTYPE html>
+<html>
+<head id="Head1" runat="server">
+    <title>DRP接口</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+     <script src="lib/jquery/jquery-1.11.1.min.js" type="text/javascript"></script>
+   
+</head>
+<body>
+    <form id="form1" runat="server" target="_self">
+        <div id="divWait" >正在打开该功能，请稍候……
+		</div>
+        <div>
+     <span id="spanM" style="color: Red"></span>
+    </div>
+    </form>
+</body>
+</html>
+
+<script type="text/javascript">
+    $("#divWait").text("");
+</script>
---
+++ Audit/JumpDrpLogin.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+namespace Audit
+{
+    public partial class JumpDrpLogin : System.Web.UI.Page
+    {
+        public string Jump = "", Url = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string serverlogin = ConfigurationManager.AppSettings["Drplogin"].ToString();
+            string p_Reurl = Request.QueryString["p_Reurl"].ToString().Trim();
+            string p_User = CtTool.SessoinUtil.GetCurrentUser().Code;
+            string str3 = p_Reurl.Replace("^", "&");
+            StringBuilder url = new StringBuilder();
+            string Drp_Token = "1901B71DA48798005FBD60F78A7DD3A7";//haijf010
+                                                                  // string Drp_Token = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile("tjinspurok", "MD5");
+
+
+            url.AppendFormat("http://{0}/DrpLogin/GenerSoftErpLogin.aspx?Drp_Token={2}&dbid=0001&userId={3}&pass=&logdate=&logintype=1&logad=&FuncNo={4}"
+                                         , serverlogin
+                                         , Drp_Token
+                                         , p_User
+                                         , p_Reurl
+                                         );
+            Response.Redirect(url.ToString());
+
+
+        }
+    }
+}
